c3f3063c7093817204e82284423c8b9ee454e304
Jamienorthman <mikoann88@gmail.com>
2023-10-15 11:00:27 +0200

    password options added in html

--- app.js
+++ app.js
@@ -22,14 +22,20 @@ document.addEventListener('click', function(event) {
 const password1El = document.getElementById("password1");
 const password2El = document.getElementById("password2");
 
-//const checked = document.getElementById("checkbox").checked
-
 // Getting the button element and adding a click event listener,
 // which passes the final function displayPasswords:
 const button = document.getElementById("generate-button");
 button.addEventListener("click", displayPasswords);
 
-// Generates a random character from the array:
+// A function to display the passwords in their divs:
+function displayPasswords() {
+  const generatedPasswordOne = generateRandomPassword();
+  const generatedPasswordTwo = generateRandomPassword();
+  password1El.textContent = generatedPasswordOne;
+  password2El.textContent = generatedPasswordTwo;
+}
+
+//Generates a random character from the array:
 const getRandomCharacter = () => { 
   let randomChar = Math.floor(Math.random() * characters.length);
   return characters[randomChar];
@@ -46,12 +52,6 @@ const generateRandomPassword = () => {
     return randomPassword;
 };
 
-// A function to display the passwords in their divs:
-function displayPasswords() {
-  const generatedPasswordOne = generateRandomPassword();
-  const generatedPasswordTwo = generateRandomPassword();
-  password1El.textContent = generatedPasswordOne;
-  password2El.textContent = generatedPasswordTwo;
-}
+
 
 
